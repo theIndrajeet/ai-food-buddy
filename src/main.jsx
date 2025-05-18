0c91f473ae9f7c2654f44f4462c1fcc9a925ebdc
TheIndrajeet <heyjeetttt@icloud.com>
2025-05-18 14:00:11 +0530

    Initial commit - project setup with blank screen issue

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import './styles/tailwind.css'; // Your new line
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+// any other existing default imports like './index.css' if it's there
+
+// ... rest of the file
